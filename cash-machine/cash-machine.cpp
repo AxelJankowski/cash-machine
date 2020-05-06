6b4945adb2457e7c20b559ea72915163b054a743
Axel <axeljankowskii@gmail.com>
2020-05-06 11:12:46 +0200

    First commit.

---
+++ cash-machine/cash-machine.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string PIN;
+
+int main()
+{
+    cout << "Welcome to our bank! We will rob you of all the money!" << endl;
+    cout << "Enter your PIN number: ";
+    cin >> PIN;
+
+    if( PIN == "1729" )
+    {
+        cout << "Correct PIN.";
+    }
+
+    return 0;
+}
